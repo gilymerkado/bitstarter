20a64947f6ae22a0650a46749075fb8083e6150b
Gily Merkado <gilymerkado@gmail.com>
2013-07-25 16:58:10 +0000

    Edited web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var index = fs.readFileSync('index.html');
+    response.send(index.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;

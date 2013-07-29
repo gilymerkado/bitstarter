36c60c05e4ae52f524a2dc597d9b800df9c779d9
Gily Merkado <gilymerkado@gmail.com>
2013-07-29 16:46:57 +0000

    Updated grader.js and checks.json

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "check.js";
+var URL_DEFAULT = "http://http://afternoon-bastion-5290.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,11 @@ var assertFileExists = function(infile) {
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
 	}
     return instr;
+};
+
+var assertUrlExists = function(url) {
+    var data = rest.get(url);
+    return data;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +72,7 @@ if(require.main == module) {
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'Address of html file', clone(assertUrlExists), URL_DEFAULT)
     .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
